e2aeaf3169b14e9e263c7b5e943d02f02c598a5b
@2381564l <2381564l@student.gla.ac.uk>
2021-02-01 12:19:15 -0500

    added difference between inward and outward facing rays

--- hittable.h
+++ hittable.h
@@ -3,10 +3,18 @@
 
 #include "ray.h"
 
+//class for objects that interact with rays
+
 struct hit_record {
     point3 p;
     vec3 normal;
+    bool front_face;
     double t;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal :-outward_normal;
+    }
 };
 
 class hittable {
