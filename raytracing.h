d9a80a1c218a7fc1a6d9cbaa2d12d1b93f85501f
@2381564l <2381564l@student.gla.ac.uk>
2021-02-03 12:34:38 -0500

    added random number generation for antialiasing

--- raytracing.h
+++ raytracing.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include<limits>
 #include<memory>
+#include<random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -18,6 +19,12 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.);
+    static std::mt19937 genertator;
+    return distribution(genertator)
 }
 
 //useful headers 
